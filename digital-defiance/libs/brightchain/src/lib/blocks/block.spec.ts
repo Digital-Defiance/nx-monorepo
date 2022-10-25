58abe63ff788a42b519354a3f53256f65d1bc85b
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-10-25 00:33:33 +0000

    code cleanup

--- digital-defiance/libs/brightchain/src/lib/blocks/block.spec.ts
+++ digital-defiance/libs/brightchain/src/lib/blocks/block.spec.ts
@@ -8,7 +8,6 @@ import BlockSize, { blockSizeToLength, blockSizes } from './blockSizes';
 
 const traceLog: Array<TraceBreadCrumb> = [];
 class TraceBreadCrumb {
-
   public readonly date: Date;
   public readonly functionName: string;
   public readonly functionArgs: Array<any>;
@@ -18,14 +17,23 @@ class TraceBreadCrumb {
     this.functionArgs = args;
     traceLog.push(this);
   }
-  public static trace(functionName: string, ...args: Array<any>): TraceBreadCrumb {
+  public static trace(
+    functionName: string,
+    ...args: Array<any>
+  ): TraceBreadCrumb {
     return new TraceBreadCrumb(functionName, ...args);
   }
   public trace(...args: Array<any>): TraceBreadCrumb {
     return new TraceBreadCrumb(this.functionName, ...args);
   }
-  public deeperTrace(functionName: string, ...args: Array<any>): TraceBreadCrumb {
-    return new TraceBreadCrumb([this.functionName,functionName].join('>'), ...args);
+  public deeperTrace(
+    functionName: string,
+    ...args: Array<any>
+  ): TraceBreadCrumb {
+    return new TraceBreadCrumb(
+      [this.functionName, functionName].join('>'),
+      ...args
+    );
   }
 }
 const pTrace = TraceBreadCrumb.trace('block.spec.ts');
@@ -53,7 +61,7 @@ pTrace.trace('bob created', bob);
 describe('block', () => {
   const blockTrace = pTrace.deeperTrace('describe block');
   it('should create a block', (done) => {
-    const itTrace= blockTrace.deeperTrace('it should create a block');
+    const itTrace = blockTrace.deeperTrace('it should create a block');
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize));
     const checksum = StaticHelpersChecksum.calculateChecksum(Buffer.from(data));
@@ -70,10 +78,12 @@ describe('block', () => {
       StaticHelpers.Uint8ArrayToUuidV4(alice.id)
     );
     expect(block.dateCreated).toEqual(dateCreated);
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
   it('should convert a block to json and back', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should convert a block to json and back');
+    const itTrace = blockTrace.deeperTrace(
+      'it should convert a block to json and back'
+    );
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize));
     const checksum = StaticHelpersChecksum.calculateChecksum(Buffer.from(data));
@@ -92,10 +102,12 @@ describe('block', () => {
     expect(rebuiltBlock.createdBy).toEqual(block.createdBy);
     expect(rebuiltBlock.createdById).toEqual(block.createdById);
     expect(rebuiltBlock.dateCreated).toEqual(block.dateCreated);
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
   it('should convert a block to json and fail to convert back with a bad member source', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should convert a block to json and fail to convert back with a bad member source');
+    const itTrace = blockTrace.deeperTrace(
+      'it should convert a block to json and fail to convert back with a bad member source'
+    );
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize));
     const checksum = StaticHelpersChecksum.calculateChecksum(Buffer.from(data));
@@ -108,10 +120,12 @@ describe('block', () => {
     expect(() => Block.fromJSON(json, (memberId: Uint8Array) => bob)).toThrow(
       'Member mismatch'
     );
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
   it('should throw when given a bad checksum', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should throw when given a bad checksum');
+    const itTrace = blockTrace.deeperTrace(
+      'it should throw when given a bad checksum'
+    );
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize));
     const dateCreated = new Date();
@@ -119,33 +133,41 @@ describe('block', () => {
     const badChecksum = StaticHelpersChecksum.calculateChecksum(
       Buffer.from(badData)
     );
-    itTrace.trace('block data generated, creating block with checksum mismatch')
+    itTrace.trace(
+      'block data generated, creating block with checksum mismatch'
+    );
     expect(() => new Block(alice, data, dateCreated, badChecksum)).toThrow(
       'Checksum mismatch'
     );
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
   it('should throw when making an empty block', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should throw when making an empty block');
+    const itTrace = blockTrace.deeperTrace(
+      'it should throw when making an empty block'
+    );
     const data = Buffer.from(new Uint8Array());
     const dateCreated = new Date();
     expect(() => new Block(alice, data, dateCreated)).toThrow(
       `Data length ${data.length} is not a valid block size`
     );
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
   it('should throw when making a block of a bad size', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should throw when making a block of a bad size');
+    const itTrace = blockTrace.deeperTrace(
+      'it should throw when making a block of a bad size'
+    );
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize) + 1);
     const dateCreated = new Date();
     expect(() => new Block(alice, data, dateCreated)).toThrow(
       `Data length ${data.length} is not a valid block size`
     );
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
-  it('should make dateCreated valus when not provided', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should make dateCreated valus when not provided');
+  it('should make dateCreated valus when not provided', () => {
+    const itTrace = blockTrace.deeperTrace(
+      'it should make dateCreated valus when not provided'
+    );
     const blockSize = randomBlockSize();
     const data = randomBytes(blockSizeToLength(blockSize));
     const checksum = StaticHelpersChecksum.calculateChecksum(Buffer.from(data));
@@ -158,19 +180,23 @@ describe('block', () => {
     const delta = Math.abs(block.dateCreated.getTime() - dateCreated.getTime());
     expect(delta).toBeLessThan(1000);
     expect(delta).toBeGreaterThanOrEqual(0);
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
-  it('should not xor with different block sizes', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should not xor with different block sizes');
+  it('should not xor with different block sizes', () => {
+    const itTrace = blockTrace.deeperTrace(
+      'it should not xor with different block sizes'
+    );
     const blockA = new Block(alice, randomBytes(BlockSize.Tiny), new Date());
     itTrace.trace('blockA created');
     const blockB = new Block(alice, randomBytes(BlockSize.Nano), new Date());
     itTrace.trace('blockB created');
     expect(() => blockA.xor(blockB, alice)).toThrow('Block sizes do not match');
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
-  it('should xor with same block sizes', (done) => {
-    const itTrace = blockTrace.deeperTrace('it should xor with same block sizes');
+  it('should xor with same block sizes', () => {
+    const itTrace = blockTrace.deeperTrace(
+      'it should xor with same block sizes'
+    );
     const blockLength: number = BlockSize.Nano;
     const blockA = new Block(alice, randomBytes(blockLength), new Date());
     itTrace.trace('blockA created');
@@ -187,10 +213,10 @@ describe('block', () => {
     expect(blockC.id).toEqual(
       Buffer.from(StaticHelpersChecksum.calculateChecksum(expectedData))
     );
-    itTrace.trace('returning from test')
+    itTrace.trace('returning from test');
   });
-  pTrace.trace('returning from all tests')
-console.log(traceLog);
+  pTrace.trace('returning from all tests');
+  console.log(traceLog);
 });
 pTrace.trace('returned');
 console.log(traceLog[traceLog.length - 1]);
